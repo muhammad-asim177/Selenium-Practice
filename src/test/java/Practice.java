ba942eb4a906b9f9dead18a6e326bc3b36cd94df
muhamadasim1996 <muhammad.asim@tanbits.org>
2025-09-17 15:05:28 +0500

    New

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -32,6 +32,15 @@ public class Practice {
         System.out.println(idValue);
     }
 
+    @Test
+    public void getT(){
+
+        driver.get("https://staging.healthdesk.ai/login/previous_request_path=%2F");
+        String text=driver.findElement(By.xpath("//button[text()='Submit']")).getText();
+        System.out.println(text);
+        String idValue=driver.findElement(By.xpath("//label[text()='Phone number or email']")).getAttribute("for");
+        System.out.println(idValue);
+    }
 
 
     @AfterClass
